80bea6819d279dcb3622216e660174c43b6adbc5
ThalesFortes <thales_pro@outlook.com>
2025-05-18 01:17:11 -0300

    Create input

--- scripts/forms.js
+++ scripts/forms.js
@@ -2,27 +2,42 @@ import { itens, setItensInLocalStorage, getItensFromLocalStorage } from "./stora
 
 
 const form = document.querySelector("form");
-const list = document.querySelector("ul")
-const item = document.getElementById("newItem")
+const list = document.querySelector("ul");
+const item = document.getElementById("newItem");
 
 
 const getNewId = () =>{
-  const itens = getItensFromLocalStorage()
-  const lastID = itens[itens.length - 1]?.id
+  const itens = getItensFromLocalStorage();
+  const lastID = itens[itens.length - 1]?.id;
   return lastID ? lastID + 1 : 1;
 }
 
 const getNewItemData = (event) => {
-  const description = event.target.sucessCheck.value()
-  const id = getNewId()
-  return {id,description}
+  const description = event.target.sucessCheck.value();
+  const id = getNewId();
+  return {id,description};
 }
 
 
-const checkbox = () => {
-  const li = document.createElement("li")
-  const label = document.createElement("label")
-  //label.setAttribute(for)
+const getCheckboxInput = ({id , description , checked}) => {
+  const li = document.createElement("li");
+  li.className = "line-style";
+  
+
+  const label = document.createElement("label");
+  label.className = "flex gap-1 center checkbox-label";
+  label.setAttribute = ("for", `${id}-item`);
+
+  const input = document.createElement("input")
+  input.type = "checkbox";
+  input.name = "sucessCheck";
+  input.className = "sucessCheck"
+  input.setAttribute = ("arial-label", "Marcar como concluido");
+  input.id = `${id}-item`
+  input.checked = checked || false
+
+
+
 }
 
 
