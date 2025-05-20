0105ecea8cb6b92e80336994de9fc0fdcfad9290
ThalesFortes <thales_pro@outlook.com>
2025-05-19 22:10:43 -0300

    Id organize

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,23 +1,9 @@
-import { itens, setItensInLocalStorage, getItensFromLocalStorage } from "./storage";
+import { setItensInLocalStorage, getItensFromLocalStorage } from "./storage";
 
 
 
 const list = document.querySelector("ul");
 const item = document.getElementById("newItem");
-
-
-const getNewId = () =>{
-  const itens = getItensFromLocalStorage();
-  const lastID = itens[itens.length - 1]?.id;
-  return lastID ? lastID + 1 : 1;
-}
-
-const getNewItemData = (event) => {
-  const description = event.target.sucessCheck.value();
-  const id = getNewId();
-  return {id,description};
-}
-
 
 
 
--- scripts/storage.js
+++ scripts/storage.js
@@ -6,3 +6,17 @@ export const getItensFromLocalStorage = () => {
   const localItens = JSON.parse(window.localStorage.getItem("itens"))
   return localItens ? localItens : []
 }
+
+export const getNewId = () =>{
+  const itens = getItensFromLocalStorage();
+  const lastID = itens[itens.length - 1]?.id;
+  return lastID ? lastID + 1 : 1;
+}
+
+export const getNewItemData = (event) => {
+  const description = event.target.sucessCheck.value();
+  const id = getNewId();
+  return {id,description};
+}
+
+
